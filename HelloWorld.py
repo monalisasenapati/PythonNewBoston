b675c326b279d0db3d7b76f4e5eb5c8da2373e32
Monalisa <monalisa_senapati@intuit.com>
2020-09-25 00:17:33 -0400

    strings and numbers

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,4 @@
+#Numbers
 print ("Hello World_new")
 a=3+4
 b=5*20
@@ -19,4 +20,18 @@ print(f)
 print(g)
 print(h)
 print(i)
+
+#Strings
+print(r'users\msenapati')
+print('I don\'t know')
+print("I don't know")
+user='monalisa senapati'
+print(len(user))
+print(user[0])
+print(user[-1])
+print(user[2:4])
+print(user[:])
+print(user[3])
+
+
 

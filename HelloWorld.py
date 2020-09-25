cab78f2307f93227c351899a5e7711e82eba8604
Monalisa <monalisa_senapati@intuit.com>
2020-09-24 23:57:16 -0400

    simple calc

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,22 @@
 print ("Hello World_new")
+a=3+4
+b=5*20
+c=12/4
+d=18//4
+e=16%3
+f=5*5*5
+g=5**3
+tuna=5
+h=20+tuna
+bacon=18
+i=bacon/tuna
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+
